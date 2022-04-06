faaab3e85d9ee4fa5bc7a4c4cdb6184248a67a11
Michael Lawson <{ID}+{username}@users.noreply.github.com>
2022-04-06 11:43:58 +0100

    many improvements

--- my_wrapper_script.sh
+++ my_wrapper_script.sh
@@ -1,12 +1,12 @@
-#/bin/bash -ic "source ~/.bashrc"
 #!/bin/bash --login
+conda init bash
 conda activate python3
 # Start the first process
 echo "about the run uvicorn $(conda list)"
 #go into the "apps" folder to run uvicorn due to the way the project was set up
-cd ..
+cd apps
 echo "about the run uvicorn changed directory $(ls)"
-uvicorn backend.main:app & npm start --prefix frontend
+uvicorn backend.main:app & nginx
 # Start the second process
 # Wait for any process to exit
 wait -n
--- python/apps/backend/backend/models/base.py
+++ python/apps/backend/backend/models/base.py
@@ -5,7 +5,7 @@ import os
 
 engine = create_engine(f"postgresql://{os.environ['PG_USER']}:{os.environ['PG_PASS']}@{os.environ['PG_URL']}/{os.environ['PG_DB']}")
 
-# engine = create_engine('postgresql://usr:pass@postgres:5432/sqlalchemy')
+#engine = create_engine('postgresql://usr:pass@0.0.0.0:5432/sqlalchemy')
 
 Base = declarative_base()
 #Base.metadata.create_all(engine)
