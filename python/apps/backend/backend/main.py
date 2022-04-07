813d4345cb074288c2347ab42ef438b128840612
Michael Lawson <{ID}+{username}@users.noreply.github.com>
2022-04-07 15:24:40 +0100

    adding some extra logging

--- python/apps/backend/backend/main.py
+++ python/apps/backend/backend/main.py
@@ -122,11 +122,16 @@ async def login_redirect(auth_provider: str):
     async with exception_handling():
 
         #think this is an instance of class of GoogleAuthProvider
-        provider = await auth_providers.get_auth_provider(auth_provider)
 
+        provider = await auth_providers.get_auth_provider(auth_provider)
+        log.info(f'the provider is {provider}')
         request_uri, state_csrf_token = await provider.get_request_uri()
+        log.info(f'the request_uri is {request_uri}')
+        log.info(f'the state_csrf_token is {state_csrf_token}')
+
 
         response = RedirectResponse(url=request_uri)
+        log.info(f'the response is {response}')
 
         # Make this a secure cookie for production use
         response.set_cookie(key="state", value=f"Bearer {state_csrf_token}", httponly=True)
