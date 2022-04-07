69aa9afcf372ebb5df72244135fe4e6006de7d99
Michael Lawson <{ID}+{username}@users.noreply.github.com>
2022-04-07 14:59:19 +0100

    fixed server import problem

--- python/apps/backend/backend/models/base.py
+++ python/apps/backend/backend/models/base.py
@@ -7,12 +7,12 @@ import os
 # Remember - storing secrets in plaintext is potentially unsafe. Consider using
 # something like https://cloud.google.com/secret-manager/docs/overview to help keep
 # secrets secret.
-db_user = os.environ["DB_USER"]
-db_pass = os.environ["DB_PASS"]
-db_name = os.environ["DB_NAME"]
-instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
 
-if "DB_SOCKET_DIR" in os.environ:
+if "INSTANCE_CONNECTION_NAME" in os.environ:
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
     db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
     engine = sqlalchemy.create_engine(
 
