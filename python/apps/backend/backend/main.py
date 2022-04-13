1aad6e1357a91275ff93a5d07ce7d78d11d1e877
Michael Lawson <michaelalanlawson@gmail.com>
2022-04-13 13:45:47 +0100

    maybe authenticated

--- python/apps/backend/backend/main.py
+++ python/apps/backend/backend/main.py
@@ -5,6 +5,10 @@ import tensorflow_text
 import requests
 import json
 from pydantic import BaseModel
+import google.auth.transport.requests
+import google.oauth2.id_token
+auth_req = google.auth.transport.requests.Request()
+
 from fastapi.encoders import jsonable_encoder
 from fastapi import (
     Depends,
@@ -330,8 +334,11 @@ async def abstract_search(abstract_draft: AbstractDraft) -> AbstractDraft:
 
 @app.post("/api/get_BERT_probs")
 async def make_cpc_pred(bert_input: BERT_input) -> BERT_input:
-    probs = requests.post('http://mlflow-patentbert-6wcv5jbs7a-nw.a.run.app:8080/invocations',
-                          json={"inputs": bert_input.dict()})
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, 'https://mal-6wcv5jbs7a-nw.a.run.app')
+    Headers = {"Authorization": f"Bearer {id_token}"}
+
+    probs = requests.post('https://mlflow-patentbert-6wcv5jbs7a-nw.a.run.app/invocations',
+                          headers=Headers, json={"inputs": bert_input.dict()})
     return {'probs': json.loads(probs.text)}
 
 
